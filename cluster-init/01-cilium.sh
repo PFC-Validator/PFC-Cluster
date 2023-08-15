8dcffcaee1e300b59ff9d72318ec59b5e081aae0
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-08-15 12:54:56 -0500

    basic cilium. no rules

---
+++ cluster-init/01-cilium.sh
@@ -0,0 +1,41 @@
+## brew install cilium-cli
+
+helm repo add cilium https://helm.cilium.io/
+export KUBERNETES_API_SERVER_ADDRESS=api.medici.loan
+export KUBERNETES_API_SERVER_PORT=6443
+export CLUSTER_DOMAIN=yieldpay.finance
+
+#helm install                                         \
+#    cilium                                                      \
+#    cilium/cilium                                               \
+#    --version 1.14.0                                            \
+#    --namespace kube-system                                     \
+#    --set ipam.mode=kubernetes                                  \
+#    --set hostFirewall.enabled=true                             \
+#    --set hubble.relay.enabled=true                             \
+#    --set hubble.ui.enabled=true                                \
+#    --set hubble.peerService.clusterDomain=${CLUSTER_DOMAIN}    \
+#    --set etcd.clusterDomain=${CLUSTER_DOMAIN}                  \
+#    --set kubeProxyReplacement=strict                           \
+#    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+#    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+#    --set cgroup.autoMount.enabled=false                        \
+#    --set cgroup.hostRoot=/sys/fs/cgroup                        \
+#    --set k8sServiceHost="${KUBERNETES_API_SERVER_ADDRESS}"     \
+#    --set k8sServicePort="${KUBERNETES_API_SERVER_PORT}"
+
+
+cilium install  --set hubble.relay.enabled=true \
+       --set hubble.ui.enabled=true     \
+       --set hostFirewall.enabled=true \
+       --set hubble.metrics.enabled="{dns,drop,tcp,flow,icmp,http}"  
+
+
+cilium status --wait
+#cilium connectivity test
+#cilium hubble enable
+cilium hubble enable --ui
+exit
+kubectl apply -f ./cilium-control-plane.yaml
+
+kubectl exec -n kube-system  cilium-796pc
