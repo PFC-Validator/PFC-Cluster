afff2ccd90b582b9e7d7198097efa54d54daa6e4
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-08-16 17:52:43 -0500

    forgot the gpg command to create the secrert

--- cluster-init/02-flux.sh
+++ cluster-init/02-flux.sh
@@ -13,3 +13,10 @@ flux bootstrap github \
   --path=./clusters/main-cluster \
 
   # might need --personal for individual github accounts
+
+
+gpg2 --export-secret-keys --armor "4C653A7AD53BC071B149AC6F05C9A59CE6F61D40" |
+  kubectl create secret generic sops-gpg \
+  --namespace=flux-system \
+  --from-file=sops.asc=/dev/stdin
+  
