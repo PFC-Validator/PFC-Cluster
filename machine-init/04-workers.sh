f773b5ca795f87a4b5a18d2fea78f8fc97d9d83a
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-08-21 15:59:17 -0500

    Update 04-workers.sh
    remove name

--- machine-init/04-workers.sh
+++ machine-init/04-workers.sh
@@ -1,7 +1,7 @@
 # if token is in TTL 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubeadm join api.medici.loan:6443 --token xx.xxx \
+kubeadm join api.xxx.xxx:6443 --token xx.xxx \
 	--discovery-token-ca-cert-hash sha256:xxx
 
 # otherwise (from controller?)
