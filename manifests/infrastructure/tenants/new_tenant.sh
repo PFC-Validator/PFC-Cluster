9e2798c3b1d39b71fa717e9771c547678071ba0e
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-09-03 10:19:13 -0500

    fix gh ref

--- manifests/infrastructure/tenants/new_tenant.sh
+++ manifests/infrastructure/tenants/new_tenant.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export TENANT_NAME=mainnet
 export TENANT_NS=cosmos-mainnet
-export GITHUB_REPO=https://github.com/PFC-Validator/PFC-Cluster-Private.git
+export GITHUB_REPO=ssh://git@github.com/PFC-Validator/PFC-Cluster-Private.git
 
 mkdir -p ./${TENANT_NAME}
 
@@ -10,10 +10,12 @@ flux create tenant ${TENANT_NAME} \
   --export > ./${TENANT_NAME}/${TENANT_NAME}-tenant.yaml
 
 
+# this will generate a SSH deploy key that you manually need to add to the repo
 flux create source git ${TENANT_NAME} \
     --namespace=${TENANT_NS} \
     --url=${GITHUB_REPO} \
     --branch=main \
+    --username=PFC-Developer --password=$GITHUB_TOKEN \
     --export > ./${TENANT_NAME}/${TENANT_NAME}-git.yaml
 
 
@@ -26,4 +28,3 @@ flux create kustomization ${TENANT_NAME} \
 
 cd ./${TENANT_NAME}/ \
    && kustomize create --autodetect
-
