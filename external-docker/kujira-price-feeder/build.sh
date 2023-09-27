9257d5cce7c534f00c17a66303eff9a7126e0bb4
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-09-27 12:01:38 -0500

    push it

--- external-docker/kujira-price-feeder/build.sh
+++ external-docker/kujira-price-feeder/build.sh
@@ -4,6 +4,7 @@ if [ "$#" -ne 1 ]; then echo "usage: $0 oracle-price-feeder version " >&2; exit 
 ORACLE_VERSION="$1"
 #git clone https://github.com/Team-Kujira/oracle-price-feeder.git
 #cd oracle-price-feeder/dockerfile || exit
-docker build --progress=plain --build-arg ORACLE_VERSION=${ORACLE_VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t price-feeder .
+docker build --progress=plain --build-arg ORACLE_VERSION=${ORACLE_VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/price-feeder:${ORACLE_VERSION} .
+docker push ghcr.io/pfc-developer/price-feeder:${ORACLE_VERSION}
 # --no-cache
 #$:${ORACLE_VERSION} .
