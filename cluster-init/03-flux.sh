0f1d176bbd0cb8851cd2a97d7b8638e85bdb9852
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-10-30 07:54:06 -0500

    upgrade operator to v0.20.0-rc1

--- cluster-init/03-flux.sh
+++ cluster-init/03-flux.sh
@@ -20,3 +20,108 @@ gpg2 --export-secret-keys --armor $KUBERNETES_GPG_KEY |
   --namespace=flux-system \
   --from-file=sops.asc=/dev/stdin
   
+
+  ---
+  kubectl -n monitoring create secret generic slack-bot-token \
+--from-literal=token=xoxb-YOUR-TOKEN
+
+---
+apiVersion: notification.toolkit.fluxcd.io/v1beta2
+kind: Provider
+metadata:
+  name: slack-bot
+  namespace: flux-system
+spec:
+  type: slack
+  channel: flux
+  address: https://slack.com/api/chat.postMessage
+  secretRef:
+    name: slack-bot-token
+---
+apiVersion: notification.toolkit.fluxcd.io/v1beta2
+kind: Alert
+metadata:
+  name: on-call-webapp
+  namespace: flux-system
+spec:
+  summary: "cluster addons"
+  eventMetadata:
+    env: "production"
+    cluster: "my-cluster"
+    region: "us-east-2"
+  providerRef:
+    name: slack-bot
+  eventSeverity: info
+  eventSources:
+    - kind: GitRepository
+      name: '*'
+    - kind: Kustomization
+      name: '*'
+---
+apiVersion: notification.toolkit.fluxcd.io/v1beta2
+kind: Alert
+metadata:
+  name: flux-system
+  namespace: flux-system
+spec:
+  eventSeverity: info
+  eventSources:
+  - kind: Kustomization
+    name: '*'
+    - kind: GitRepository
+    name: '*'
+  providerRef:
+    name: slack-bot
+---
+apiVersion: notification.toolkit.fluxcd.io/v1beta2
+kind: Alert
+metadata:
+  name: slack-alert
+  namespace: flux-system
+spec:
+  eventSeverity: info
+  eventSources:
+  - kind: Kustomization
+    name: '*'
+  - kind: GitRepository
+    name: '*'
+  providerRef:
+    name: slack-bot
+---
+apiVersion: notification.toolkit.fluxcd.io/v1beta2
+kind: Alert
+metadata:
+  name: new-alert
+  namespace: flux-system
+spec:
+  eventSeverity: info
+  eventSources:
+  - kind: Kustomization
+    name: '*'
+  - kind: GitRepository
+    name: '*'
+  providerRef:
+    name: new-provider
+
+
+--
+https://discord.com/api/webhooks/1161775333574443108/ziIjCByA5xUhehMDayI9Z23CZsClzXKGlhDYsFFsQAjGZiU3jeazVuomJhzGM9TzVRwX
+
+
+kubectl -n flux-system create secret generic discord-url \
+--from-literal=address=https://discord.com/api/webhooks/1161775333574443108/ziIjCByA5xUhehMDayI9Z23CZsClzXKGlhDYsFFsQAjGZiU3jeazVuomJhzGM9TzVRwX
+
+flux create alert-provider new-provider \
+  --type discord \
+  --secret-ref discord-url \
+  --channel demo-channel \
+  --username flux-bot \
+  --export
+
+
+flux create alert new-alert \
+  --provider-ref new-provider \
+  --event-severity info \
+  --event-source Kustomization/'*' \
+  --event-source GitRepository/'*' \
+  --namespace flux-system
