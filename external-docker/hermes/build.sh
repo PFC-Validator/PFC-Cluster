b9bc9d18f28b2dbc8e252968ce7a188301a6cc2d
PFC <81114960+PFC-developer@users.noreply.github.com>
2023-11-10 09:30:51 -0600

    upgrade cosmos-operator to v0.21.1

---
+++ external-docker/hermes/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Stolen from - https://github.com/DefiantLabs/externalPackages/tree/main/kujirad
+if [ "$#" -ne 1 ]; then echo "usage: $0 oracle-price-feeder version " >&2; exit 1; fi
+ORACLE_VERSION="$1"
+#git clone https://github.com/Team-Kujira/oracle-price-feeder.git
+#cd oracle-price-feeder/dockerfile || exit
+docker build --no-cache --progress=plain --build-arg ORACLE_VERSION=${ORACLE_VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/kujira-price-feeder:${ORACLE_VERSION} .
+# --no-cache
+docker push ghcr.io/pfc-developer/kujira-price-feeder:${ORACLE_VERSION}
+#$:${ORACLE_VERSION} .
