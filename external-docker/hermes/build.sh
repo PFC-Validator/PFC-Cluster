94d243c68e568261465e74322b513d5401194521
PFC <PFC-validator@protonmail.com>
2024-03-23 14:04:05 +0000

    dockerfile updates

--- external-docker/hermes/build.sh
+++ external-docker/hermes/build.sh
@@ -2,8 +2,8 @@
 if [ "$#" -ne 1 ]; then echo "usage: $0 hermes-version " >&2; exit 1; fi
 VERSION="$1"
 
-docker build  --progress=plain --build-arg VERSION=${VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/hermes:${VERSION} .
+docker build  --progress=plain --build-arg VERSION=${VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/hermes:${VERSION}.2 .
 # --no-cache
 #docker tag ghcr.io/pfc-developer/hermes:${VERSION} ghcr.io/pfc-developer/hermes:${VERSION}.1
-docker push ghcr.io/pfc-developer/hermes:${VERSION}
+docker push ghcr.io/pfc-developer/hermes:${VERSION}.2
 
