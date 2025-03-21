a1199928129db2831cdf685ce822e473bbc72ed3
I <no-name@gmail.com>
2025-03-21 17:12:58 -0400

    add push

--- external-docker/hermes/build.sh
+++ external-docker/hermes/build.sh
@@ -2,8 +2,8 @@
 if [ "$#" -ne 1 ]; then echo "usage: $0 hermes-version " >&2; exit 1; fi
 VERSION="$1"
 
-docker build --no-cache --platform linux/x86_64 --progress=plain --build-arg VERSION=${VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/hermes:${VERSION} .
+docker build --platform linux/x86_64 --progress=plain --build-arg VERSION=${VERSION} --build-arg TARGETPLATFORM="linux/amd64" -t ghcr.io/pfc-developer/hermes:${VERSION} .
 # --no-cache
 #docker tag ghcr.io/pfc-developer/hermes:${VERSION} ghcr.io/pfc-developer/hermes:${VERSION}.1
-#docker push ghcr.io/pfc-developer/hermes:${VERSION}
+docker push ghcr.io/pfc-developer/hermes:${VERSION}
 
